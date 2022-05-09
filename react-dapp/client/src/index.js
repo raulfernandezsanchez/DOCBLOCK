06dba0acfdb9bfe76cc6a9f140cce2a3e133d70d
xgervilla <xavigervilla@MacBook-Pro-de-Xavier.local>
2022-05-09 19:45:30 +0200

    Last changes

--- react-dapp/client/src/index.js
+++ react-dapp/client/src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import {render} from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  //<React.StrictMode>
+const container = document.getElementById('root');
+render(
+  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  //</React.StrictMode>
-);
+  </React.StrictMode>
+, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
