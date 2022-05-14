d96169878703e4bae41f018b0a75163141bc16de
arnx18 <arnx18@gmail.com>
2022-05-14 19:47:04 +0200

    signup page y api

--- react-dapp/client/src/Pages/signupPage.js
+++ react-dapp/client/src/Pages/signupPage.js
@@ -16,6 +16,13 @@ export default function SignupPage() {
     const [firstname, setFirstname] = useState();
     const [lastname, setLastname] = useState()
 
+    function isRegistered (registered) {;
+      for (let i = 0; i < registered.length; ++i) {
+        if (registered[i].email == email) return true;
+      }
+      return false;
+    }
+
     async function handleSubmit (event) {
         event.preventDefault();
         //comprobacion de que los campos tienen contenido:
@@ -35,30 +42,53 @@ export default function SignupPage() {
                     },
                 });
                 result = await result.json();
-                console.log(result);
 
-                localStorage.setItem('isAuthenticated', true);
-                localStorage.setItem('isCompany', true);
-                localStorage.setItem('userID', email);
-                navigate("/homeCompany");
+                //comprobar si el usuario ya tiene una cuenta
+                if (!isRegistered(result)) {
+
+                  localStorage.setItem('isAuthenticated', true);
+                  localStorage.setItem('isCompany', true);
+                  localStorage.setItem('userID', email);
+                  navigate("/homeCompany");
+                } else {
+                  alert('This email address already exists!');
+                }
+
             }
             //login de user
             else{
                //let name = firstname.concat(" ", lastname);
                 //let item ={email,password,name}
-                let result = await fetch("https://vast-peak-05541.herokuapp.com/api/users",{
+                let result = await fetch("https://vast-peak-05541.herokuapp.com/api/users", {
                     method:'GET',
                     headers:{
                         "Content-Type":'application/json',
                     },
                 });
                 result = await result.json();
-                console.log(result);
 
-                localStorage.setItem('isAuthenticated', true);
-                localStorage.setItem('isCompany', false);
-                localStorage.setItem('userID', email);
-                navigate("/homeUser");
+                //comprobar si el usuario ya tiene una cuenta
+                if (!isRegistered(result)) {
+                  //nuevo usuario
+                  let new_user = {
+                    useremail : email,
+                    userpassword : password,
+                    username : firstname,
+                  };
+                  let result = await fetch("https://vast-peak-05541.herokuapp.com/api/users", {
+                      body: JSON.stringify(new_user),
+                      method:'POST',
+                      headers:{
+                          "Content-Type":'application/json',
+                      },
+                  });
+                  localStorage.setItem('isAuthenticated', true);
+                  localStorage.setItem('isCompany', false);
+                  localStorage.setItem('userID', email);
+                  navigate("/homeUser");
+                } else {
+                  alert('This email address already exists!');
+                }
             }
         }
     }
