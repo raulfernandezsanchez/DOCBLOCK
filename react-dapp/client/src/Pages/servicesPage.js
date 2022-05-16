b4a718af14535dce3c3715d3b5623e74c8ebc4c2
diegong21 <diego.ng212@gmail.com>
2022-05-16 13:28:26 +0200

    servicesPage

--- react-dapp/client/src/Pages/servicesPage.js
+++ react-dapp/client/src/Pages/servicesPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import '../css/servicesPage.css';
 
 import Footer from "../Components/footer";
 import NavBar from "../Components/navbar";
@@ -8,19 +9,26 @@ class ServicesPage extends Component{
         return (
             <>
             <NavBar></NavBar>
-            <div id="services" style={{'marginLeft':'20px'}}>
-                <h2>For companies</h2>
-                <ul>
-                    <li>Assign contracts to users</li>
-                    <li>Follow and control the progress</li>
-                    <li>External consult</li>
-                </ul>
-                <br/>
-                <h2>For users</h2>
-                <ul>
-                    <li>Easy sign</li>
-                    <li>Validate experience</li>
-                </ul>
+            <div className="about-section" width="100%">
+                <h1>Services</h1>
+                <p>DocBlock has different functionah2ties.</p>
+            </div>
+            <div className="row d-flex justify-content-center">
+              <div className="column">
+                <div className="cardServices">
+                  <h5>Companies</h5>
+                  <h2>Assign contracts to users.</h2>
+                  <h2>Follow and control the progress.</h2>
+                  <h2>External consult.</h2>
+                </div>
+              </div>
+              <div className="column">
+                <div className="cardServices">
+                    <h5>Users</h5>
+                    <h2>Sign your documents.</h2>
+                    <h2>Validate your experience.</h2>
+                </div>
+              </div>
             </div>
             <Footer></Footer>
             </>
