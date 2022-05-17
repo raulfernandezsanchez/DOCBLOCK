d751cdf639b0d71f06f3b3f4f7ebf2428a5158ac
xgervilla <xavigervilla@MacBook-Pro-de-Xavier.local>
2022-05-17 23:52:48 +0200

    Sign with email

--- react-dapp/client/src/Pages/userContracts.js
+++ react-dapp/client/src/Pages/userContracts.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import emailjs from 'emailjs-com';
 
 import Footer from "../Components/footer";
 import NavBarUser from "../Components/navbaruser";
@@ -6,25 +7,41 @@ import NavBarUser from "../Components/navbaruser";
 export default function UserContracts(){
     const filename = localStorage.getItem('contractFile');
     const fileContent = localStorage.getItem('contractContent');
+    //const mail = localStorage.getItem('userID');
+    let mail='infodocblock@gmail.com';
 
     const [minVal] = useState(1000000);
     const [maxVal] = useState(1000000000);
     const [randomNum, setRandomNum] = useState(0);
+    window.onload = () =>{
+        let newVal = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal);
+        setRandomNum(newVal);
+    }
 
-    const generateRandomNum = () => {
-        setRandomNum(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal));
+    const sendEmail = (e) =>{
         const firstNameInput = document.getElementById('signInput');
         const modalSigned = document.getElementById('signModal');
         modalSigned.hidden = false;
         firstNameInput.disabled = false;
+
+        var templateParams = {
+            user_email: mail,
+            number: randomNum
+        };
+        
+        e.preventDefault();
+        emailjs.send('Docblock_support', 'Signature_template', templateParams, 'XWMGER03w0eoay02e').then((result) =>{
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
     };
     
     const handleSign = () => {
         const firstNameInput = document.getElementById('signInput');
-        
+        console.log(randomNum);
         if (parseInt(firstNameInput.value,10) !== randomNum){
             alert('Wrong number, try again');
-            generateRandomNum();
         }
         else{
             const modalSigned = document.getElementById('signModal');
@@ -42,7 +59,7 @@ export default function UserContracts(){
             <h4>Pending contracts</h4>
             <div className="row">
                 <div className="col-sm-4">
-                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#multifactor" onClick={generateRandomNum}>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#multifactor" onClick={sendEmail}>
                     Sign
                     </button>
                     <div className="modal fade" id="multifactor" tabIndex="-1" aria-labelledby="multifactor" aria-hidden="true">
@@ -53,8 +70,7 @@ export default function UserContracts(){
                                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setRandomNum(0)}></button>
                                 </div>
                                 <div className="modal-body">
-                                    <p>Enter the following number to sign the document:</p>
-                                    <p>{randomNum}</p>
+                                    <p>Enter the code from your email:</p>
                                     <input type='number' id='signInput'/>
                                 </div>
                                 <div className="modal-footer">
