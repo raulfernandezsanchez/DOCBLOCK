42952c02e4d50e40d0b8a5b55065a779773c0592
diegong21 <diego.ng212@gmail.com>
2022-05-18 10:00:21 +0200

    navbar millorat

--- react-dapp/client/src/Components/navbarcompany.js
+++ react-dapp/client/src/Components/navbarcompany.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import '../css/navbar.css'
 
 const NavBarCompany = () =>{
     const handleClick = () =>{
@@ -17,7 +18,7 @@ const NavBarCompany = () =>{
                   DocBlock
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="menu">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <NavLink to="/homeCompany" className={({isActive})=>(isActive ? "nav-link active" : "nav-link")}>My page</NavLink>
--- react-dapp/client/src/Components/navbaruser.js
+++ react-dapp/client/src/Components/navbaruser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import '../css/navbar.css'
 
 const NavBarUser = () =>{
     const handleClick = () =>{
@@ -18,7 +19,7 @@ const NavBarUser = () =>{
                   DocBlock
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="menu">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <NavLink to="/homeUser" className={({isActive})=>(isActive ? "nav-link active" : "nav-link")}>My page</NavLink>
